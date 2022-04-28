1911a2511804a2c60c2bd2fd614f88417e674c04
Agandaur1 <okaresmithn@gmail.com>
2022-04-28 16:46:48 +1200

    bugfix

--- Postprocessing_Programs/GetMeanLifetimeOfAllTrials.py
+++ Postprocessing_Programs/GetMeanLifetimeOfAllTrials.py
@@ -107,7 +107,7 @@ target_data = []
 for target_energy in target_energies:
 	target_data.append(data[target_energy])
 
-sorteddata = (list(x) for x in zip(*sorted(zip(data['trial'], data['last_encounter_time'], **target_data))))
+sorteddata = (list(x) for x in zip(*sorted(zip(data['trial'], data['last_encounter_time'], *target_data))))
 print(sorteddata)
 
 
