c739c97c18e60941488446d05dd6e36cac1cdcb6
Agandaur1 <okaresmithn@gmail.com>
2022-05-04 10:37:44 +1200

    bugfix

--- Postprocessing_Programs/RemoveEmptyErrFiles.py
+++ Postprocessing_Programs/RemoveEmptyErrFiles.py
@@ -7,7 +7,6 @@ for root, dirs, files in os.walk(os.getcwd()):
 	for f in files:
 		if f.startswith("arrayJob_") and f.endswith(".err"):
 			print(os.path.join(root, f))
-			print(os.path.join(dirs, f))
 			count += 1
 
 print(count)
