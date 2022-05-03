5202cd2d5790e229707f5b69d36fe0a93810f330
Agandaur1 <okaresmithn@gmail.com>
2022-05-04 10:50:39 +1200

    bugfix

--- Postprocessing_Programs/RemoveEmptyErrFiles.py
+++ Postprocessing_Programs/RemoveEmptyErrFiles.py
@@ -3,12 +3,24 @@
 import os, sys
 
 count = 0
+to_rm = []
 for root, dirs, files in os.walk(os.getcwd()):
 	for f in files:
 		if f.startswith("arrayJob_") and f.endswith(".err"):
 			fpath = os.path.join(root, f)
 			if os.stat(fpath).st_size == 0:
-				print(fpath)
+				to_rm.append(fpath)
 				count += 1
 
-print(count)
+print("%d files are marked for deletion. If this sounds correct, please input the following:" % count)
+print("\t\'DELETE%d\'" % count)
+code = input()
+if code == "DELETE%d" % count:
+	for f in to_rm:
+		os.remove(fpath)
+	print("%d files deleted." % count)
+else:
+	print("No files were deleted.")
+
+
+
