32222af00a53cacbab766e66e80fa25389fa2cf4
Agandaur1 <okaresmithn@gmail.com>
2022-05-25 11:01:12 +1200

    Added script to print out non-lazy censored data

---
+++ SurvivalAnalysis/AppendTrialsToDecayData.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+path = os.getcwd()
+cluster_makeup = path.split('basin_hopping_trials/')[1].split('/')[0]
+print("%s was idendified as the current cluster. Is this correct [Y/n]: ", end='')
+correct_cluster = None
+while correct_cluster 
+mode = None
+print("Please name the mode you are appending data for: ", end='')
+while mode is None:
+	mode = input()
+
+
+
